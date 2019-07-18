3811e6ff2b986bec3b9eb277692df67aaa2c7e39
G Sudarshan <43257931+G-Sudarshan@users.noreply.github.com>
2019-07-18 22:08:45 +0530

    Add files via upload

---
+++ 1. opendir_and_readdir.c
@@ -0,0 +1,52 @@
+//Program for implementation of opendir() and readdir()
+
+//Github link : https://github.com/G-Sudarshan/
+
+// 16 07 2019
+
+// first practical program [ for GP Nashik CM Department ]
+
+
+#include <stdio.h>
+#include <dirent.h> // Required header file for opendir() and readdir() functions
+
+int main()
+{
+	struct dirent *dp; // Through this we will perform all functions . It is like an object, But it is actually a pointer.
+
+	DIR *dr = opendir("C:/Program Files/");
+
+	/* Here you can give any location and it will be stored This statement of
+       opendir() returns pointer of DIR type.
+	   Don't use backward slash'\' while writing location it may result in error because
+	   compiler consider it escape sequence like \n */
+
+	if (dr == NULL) // if location you entered do exist then  opendir() will return NULL
+	{
+		printf("Could not open current directory" );
+		return 0; // Return statement terminates whole main function here and also whole program
+	}
+
+
+	while ((dp = readdir(dr)) != NULL)   // Like file handling program we read next character from file until EOF. The next directory name ( Folder or File name ) is read till no more dir are remaining
+			printf("%s\n", dp->d_name);
+
+			/* Actual struct dirent is defined in header file as follow hence you de->d_name is used
+
+			   struct dirent
+               {
+	              long		d_ino;		//Always zero.
+                  unsigned short	d_reclen;	//Always zero.
+	              unsigned short	d_namlen;	//Length of name in d_name.
+	              char		d_name[FILENAME_MAX]; // File name.
+                };
+
+
+			*/
+
+	closedir(dr); // To close dir
+	return 0;
+}
+
+// Official Documentation By UNIX is available here http://pubs.opengroup.org/onlinepubs/7990989775/xsh/readdir.html for readdir()
+
---
+++ 2 . first_come_first_serve.c
@@ -0,0 +1,107 @@
+// C program for implementation of First Come First Serve Scheduling Algorithm ( for Non pre-emptive processes )
+
+//Github link : https://github.com/G-Sudarshan/
+
+// 16 07 2019
+
+// Second practical program [ for GP Nashik CM Department ]
+
+
+#include<stdio.h>
+
+struct process  // C is not object oriented hence we are using structure to use it like class object. So We can access individual process's attributes
+{
+    char p_name[10]; // Name of process
+    int order;       // Order in which the process arrived
+    int burst_time;  // burst time is time required for a process to execute
+    int c_time;      // completion time is time on which process is terminated / completed
+    int a_time;      // arrival time is time at which process arrives for execution
+    int tat;         // TAT turn around time is time for which process was in CPU it is burst time + waiting time
+    int w_time;      // waiting time is time in which process is nor=t in CPU it may be in blocked state for I/O etc.
+};
+
+int main()
+{
+    float awt; //average waiting time for all processes
+    int no, i, total_w_time = 0,j;
+
+    /*
+       no for no. of processes
+       i for for loop
+       total_w_time is total waiting time which is 0 at beginning
+       j is for for loop
+
+    */
+
+
+    printf("Enter no. of processes to be simulated for this program : ");
+    scanf("%d",&no);
+    struct process p[no];
+
+     // Take input from user for details of processes
+
+    for(i = 0 ; i < no ; i++)
+    {
+        printf("Enter name of process %d : ",i+1);
+        scanf("%s",&p[i].p_name);
+
+        printf("Enter order of process %d (Order in which process will come) : ",i+1);
+        scanf("%d",&p[i].order);
+
+        printf("Enter burst time of process %d : ",i+1);
+        scanf("%d",&p[i].burst_time);
+    }
+
+    // In following nested for loop we are performing bubble sort to sort processes in ascending order according to their order attribute
+
+     for(i = 0 ; i < no ; i++)
+     {
+         for(j = 0 ; j < no-i-1 ; j++)
+        {
+            if(p[j].order > p[j+1].order )
+            {
+              struct process temp;
+
+              temp=p[j];
+              p[j]=p[j+1];
+              p[j+1]=temp;
+
+            }
+        }
+     }
+
+     p[0].a_time = 0; // first process will arrive at 0
+     p[0].c_time = p[0].burst_time;
+     p[0].w_time = 0;
+
+      // In following for loop we are calculating arrival time, completion time and waiting for each process
+     for(i = 1 ; i < no ; i++)
+     {
+         p[i].a_time = p[i-1].c_time;
+         p[i].c_time = p[i].a_time + p[i].burst_time;
+         p[i].w_time = p[i-1].w_time + p[i-1].burst_time;
+
+     }
+
+     //calculating total waiting time for finding out average waiting time
+     for(i = 0 ; i < no ; i++)
+     {
+         total_w_time = total_w_time + p[i].w_time;
+     }
+
+     awt = total_w_time / no;
+
+
+    // Printing values in tabular format
+    printf("values are : \n\n\n\n");
+    printf("\n\nprocess name   order     burst time   waiting time   \n\n"); // you can also print arrival and completion time here
+
+    for(i = 0 ; i < no ; i++ )
+    {
+    printf("   %s            %d           %d             %d       \n\n",p[i].p_name,p[i].order,p[i].burst_time,p[i].w_time);
+    }
+
+    printf("\n\n\n\n\n  Average Waiting Time is %f Time units  \n\n\n\n\n\n",awt);
+
+    return 0;
+}
